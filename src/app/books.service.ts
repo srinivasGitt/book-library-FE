d83878ae465a59ea64c9de0473b8704dda091d22
srinivas <srinivas.gollapalli@anuyat.com>
2024-04-22 15:04:04 +0530

    updated environment variables

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,37 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  serverURL = "http://localhost:3001/books/"
+  serverURL = environment.serviceURL
 
   constructor(private http :HttpClient) { }
 
   //add book
   addBook(bookData: any){
-    return this.http.post(this.serverURL, bookData)
+    return this.http.post(`${this.serverURL}/books`, bookData);
   }
 
   //get all the books
   getBooks(){
-    return this.http.get(this.serverURL);
+    return this.http.get(`${this.serverURL}/books`);
   }
 
   //get book by isbn
   getBookByIsbn(isbn: string){
-    return this.http.get<any>(`${this.serverURL}${isbn}`);
+    return this.http.get<any>(`${this.serverURL}/books${isbn}`);
   }
 
   //edit a book using isbn number
   editBook(isbn: string, newData: any){
-    return this.http.put(`${this.serverURL}${isbn}`, newData);
+    return this.http.put(`${this.serverURL}/books${isbn}`, newData);
   }
 
   // Delete a book by ISBN number
   deleteBook(isbn: string){
-    return this.http.delete(`${this.serverURL}${isbn}`);
+    return this.http.delete(`${this.serverURL}/books${isbn}`);
   }
 }
